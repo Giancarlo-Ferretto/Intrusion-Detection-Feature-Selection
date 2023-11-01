b6ba853b8f6d77d44acedf4e52404e84a4961e05
Giancarlo Ferretto <81869512+Giancarlo-Ferretto@users.noreply.github.com>
2023-11-01 13:51:01 -0300

    subir fuentes

---
+++ src/redundancy_svd.py
@@ -0,0 +1,13 @@
+# My Utility : 
+
+import numpy  as np   
+
+# normalization of the data
+def norm_data():
+    ...    
+    return()
+
+# SVD of the data
+def svd_data():
+    ...    
+    return()
